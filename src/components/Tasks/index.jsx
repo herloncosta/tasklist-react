035acabfe3acc9a0b4c41fb4bf0a02e991d9be51
herloncosta <herlon36@gmail.com>
2022-10-29 13:25:40 -0300

    componentizando tarefas

---
+++ src/components/Tasks/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { FaEdit, FaWindowClose } from "react-icons/fa";
+
+import "./Tasks.css";
+
+export function Tasks({ tarefas, handleEdit, handleDelete }) {
+	return (
+		<ul className="tasks">
+			{tarefas.map((tarefa, index) => (
+				<li key={tarefa}>
+					{tarefa}
+					<span>
+						<FaEdit onClick={(e) => handleEdit(e, index)} className="edit" />
+						<FaWindowClose
+							onClick={(e) => handleDelete(e, index)}
+							className="delete"
+						/>
+					</span>
+				</li>
+			))}
+		</ul>
+	);
+}
+
+Tasks.propTypes = {
+	tarefas: PropTypes.array.isRequired,
+	handleEdit: PropTypes.func.isRequired,
+	handleDelete: PropTypes.func.isRequired,
+};
